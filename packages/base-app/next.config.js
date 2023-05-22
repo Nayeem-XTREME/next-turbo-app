83db6b0b7d054713583257bd36918a21cf385bd9
Niaz Morshed Nayeem <nayeemxtreme@live.com>
2023-05-23 03:19:48 +0600

    init 2

---
+++ packages/base-app/next.config.js
@@ -0,0 +1,16 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+
+const config = {
+  reactStrictMode: true,
+  transpilePackages: ['ui'],
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['i.dummyjson.com', 'i.vimeocdn.com'],
+  },
+};
+
+module.exports = withBundleAnalyzer(config);
