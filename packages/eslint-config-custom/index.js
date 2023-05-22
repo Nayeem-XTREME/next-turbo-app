f973e42921d0c60c56c9874835990177c515b14a
Niaz Morshed Nayeem <nayeemxtreme@live.com>
2023-05-23 03:50:56 +0600

    init 3

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,109 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
-  rules: {
-    '@next/next/no-html-link-for-pages': 'off',
+  env: {
+    es2021: true,
+    browser: true,
   },
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+    ecmaVersion: 'latest',
     babelOptions: {
       presets: [require.resolve('next/babel')],
     },
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'react/display-name': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/exports': 'error',
+    '@next/next/no-html-link-for-pages': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        // Copied and tweaked from
+        // https://github.com/lydell/eslint-plugin-simple-import-sort/blob/main/examples/.eslintrc.js
+        groups: [
+          // Node.js builtins. You could also generate this regex if you use a `.js` config.
+          // For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+          ],
+          // Packages. `next` and `react` related packages come first.
+          [
+            '^react',
+            'next',
+            'next-auth',
+            'next-auth/providers',
+            'next-auth/next',
+            'tss-react',
+            'tailwind-styled-components',
+            'tailwind.config.js',
+            'tailwindcss/resolveConfig',
+            'date-fns',
+            'react-icons',
+            'react-hook-form',
+            '@emotion',
+            '@next',
+            '@mui',
+            '@hookform/resolvers',
+            '@reduxjs/toolkit',
+            'redux-persist',
+            'redux-logger',
+            'next-redux-cookie-wrapper',
+            'next-redux-wrapper',
+            '^@?\\w',
+          ],
+          // pages
+          // components
+          // hooks
+          // api
+          // state
+          // helpers
+          // utils
+          // configs
+          // types
+          [
+            '^(@/app)(/.*|$)',
+            '^(@/pages)(/.*|$)',
+            '^(@/components)(/.*|$)',
+            '^(@/hooks)(/.*|$)',
+            '^(@/api)(/.*|$)',
+            '^(@/state)(/.*|$)',
+            '^(@/helpers)(/.*|$)',
+            '^(@/utils)(/.*|$)',
+            '^(@/configs)(/.*|$)',
+            '^(@/types)(/.*|$)',
+          ],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // public
+          ['^(public)(/.*|$)'],
+          // types
+          ['^.*\\u0000$'],
+          // Style module imports.
+          ['^.+\\.module.scss$', '^.+\\.module.css$'],
+          // Style imports.
+          ['^.+\\.scss$', '^.+\\.css$'],
+          // Side effect imports.
+          ['^\\u0000'],
+        ],
+      },
+    ],
   },
 };
