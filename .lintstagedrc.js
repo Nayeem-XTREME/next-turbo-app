ca3055fa0de12092a92da0767beefefec1207d3b
Niaz Morshed Nayeem <nayeemxtreme@live.com>
2023-05-28 02:44:03 +0600

    Test husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // For tsc, see https://github.com/okonet/lint-staged/issues/825#issuecomment-620018284
+  '*.{ts,tsx}': () => 'npm run check-types',
+  '*.{js,ts,tsx}': 'npm run check-lint',
+  // same as tsc, lint-stages was ignoring the project prettier config
+  '*.{js,ts,tsx,css,json}': () => 'npm run check-format',
+};
